37c0e46f423bf59b4f561d21c331c5ed753c041f
Puneeth2410 <84957587+Puneeth2410@users.noreply.github.com>
2021-06-12 14:02:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+seaImg=loadImg("sea.png");
+shipImg=loadAnimation(ship1.png);
+spriteName=addImg(seaImg);
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+if(sea.x < 0){
+sea.x=sea.width/2
+}
+}
+sea.velocityX=2;
